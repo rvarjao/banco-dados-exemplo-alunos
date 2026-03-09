6bfd56f9e77f896ab488388fb43a0f6fa8b64082
Ricardo Varjão <rvarjao@gmail.com>
2026-03-09 10:52:05 -0300

    Create exemplos-comandos.sql

---
+++ exemplos-comandos.sql
@@ -0,0 +1,40 @@
+ -- UPDATE / ATUALIZACAO
+UPDATE
+	alunos
+SET
+	nome = 'Fulano'
+WHERE id = 1;
+
+
+SELECT *
+FROM alunos
+ORDER BY id
+LIMIT 10
+OFFSET 20;
+ 
+
+-- SELECAO DOS DADOS
+SELECT *
+FROM
+alunos
+	WHERE
+    	 data_nascimento >= '2010-01-01'
+ ORDER BY nome;
+ 
+ --
+
+-- REMOVE REGISTROS
+ DELETE
+ FROM
+ 	alunos
+WHERE turma_id = 11;
+
+DELETE FROM alunos
+WHERE email = 'imercalli@example.org';
+
+SELECT * FROM alunos
+WHERE turma_id = 11;
+
+
+
+
